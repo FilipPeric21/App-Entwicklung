625d29db7434cbb543668841bfa0530e33a03043
Filip_Peric <f.peric@students.htl-leonding.ac.at>
2026-03-10 11:25:34 +0100

    Verbindung von JS zu HTML

---
+++ generator.js
@@ -0,0 +1,11 @@
+//sucht alle wichtigen Elemente anhand ihrer ID
+const colorPicker = document.getElementById('baseColor');
+const previewBox = document.getElementById('preview-box');
+const previewText = document.getElementById('preview-text');
+const btnPrimary = document.getElementById('preview-button-primary');
+const btnAccent = document.getElementById('preview-button-accent');
+
+// Die Text-Labels in denen die Hexcodes stehen werden
+const labelSecondary = document.getElementById('label-secondary');
+const labelAccent = document.getElementById('label-accent');
+const labelBg = document.getElementById('label-bg');
