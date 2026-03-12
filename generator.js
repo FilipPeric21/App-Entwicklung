fbb53ac77e9bd2c2487d469cf0abb0a437718132
Filip_Peric <f.peric@students.htl-leonding.ac.at>
2026-03-12 10:11:24 +0100

    Farbberechnung und Live-Vorschau hinzugefügt

--- generator.js
+++ generator.js
@@ -9,3 +9,29 @@ const btnAccent = document.getElementById('preview-button-accent');
 const labelSecondary = document.getElementById('label-secondary');
 const labelAccent = document.getElementById('label-accent');
 const labelBg = document.getElementById('label-bg');
+
+
+colorPicker.addEventListener('input', () => {
+    const primaryColor = colorPicker.value; // Die gewählte Farbe (z.B. #00ff00)
+
+    // Berechnugen
+    const secondaryColor = adjustBrightness(primaryColor, -20); 
+    const accentColor = getComplementaryColor(primaryColor);    
+    const backgroundColor = adjustBrightness(primaryColor, 90);  
+
+    // UI aktualisieren (Farben auf der Seite anwenden)
+    previewBox.style.backgroundColor = backgroundColor;
+    
+    btnPrimary.style.backgroundColor = primaryColor;
+    btnPrimary.style.color = getContrastColor(primaryColor);
+
+    btnAccent.style.backgroundColor = accentColor;
+    btnAccent.style.color = getContrastColor(accentColor);
+
+    previewText.style.color = primaryColor;
+
+    // Die Hex-Codes in die Labels schreiben
+    labelSecondary.textContent = secondaryColor;
+    labelAccent.textContent = accentColor;
+    labelBg.textContent = backgroundColor;
+});
